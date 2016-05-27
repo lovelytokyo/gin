198bd03c8298c412b7f6c5a5b10a4b659b5e7386
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-27 16:35:41 +0900

    modify

--- gin/authentication.go
+++ gin/authentication.go
@@ -1,1 +1,56 @@
-package gin
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func responseWithError(code int, message string, c *gin.Context) {
+	resp := map[string] string{
+		"error": message,
+	}
+
+	c.JSON(code, resp)
+	c.Abort()
+}
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("api_token")
+
+		if token == "" {
+			responseWithError(401, "API token required", c)
+			return
+		}
+
+		if token != os.Getenv("API_TOKEN") {
+			responseWithError(401, "Invalid API token", c)
+			return
+		}
+		c.Next()
+	}
+}
+
+func main() {
+	os.Setenv("API_TOKEN", "token_value")
+
+	r := gin.Default()
+	r.Use(TokenAuthMiddleware())
+
+	r.GET("/ranking", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"1" : "Mitsubishi",
+			"2" : "Hitachi",
+			"3" : "Sony",
+		})
+	})
+
+	r.GET("/news", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"date" : "2016/05/27",
+			"title" : "hogehoge",
+			"detail" : "pepepepe",
+		})
+	})
+	r.Run()
+}
--- gin/middleware.go
+++ gin/middleware.go
@@ -1,1 +1,39 @@
-package gin
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+)
+
+func Logger() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		t := time.Now()
+
+		// set example valiable
+		c.Set("example", "12345")
+
+		c.Next()
+
+		// after request
+		latency := time.Since(t)
+		log.Println(latency)
+
+		// access the status we are sending
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
+
+func main()  {
+	r := gin.New()
+	r.Use(Logger()) // custom middlware
+
+	r.GET("/test", func (c *gin.Context) {
+		example := c.MustGet("example").(string)
+
+		log.Println(example)
+	})
+
+	r.Run(":8080")
+}
