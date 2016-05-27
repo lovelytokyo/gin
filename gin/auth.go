40e319505ed8b65e030182fab3a173323a1a3545
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-27 15:44:10 +0900

    add redirect and abort

--- gin/auth.go
+++ gin/auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 var secrets = gin.H{
@@ -14,6 +15,23 @@ var secrets = gin.H{
 		"email": "austin@bar.com",
 		"phone": "66666",
 	},
+}
+
+func TokenAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("token")
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "token required"})
+			c.Abort()
+		}
+
+		os.Setenv("TOKEN", "tokentest")
+		if token != os.Getenv("TOKEN") {
+			c.Redirect(http.StatusMovedPermanently, "/readme")
+			c.Abort()
+		}
+		c.Next()
+	}
 }
 
 func main () {
@@ -33,6 +51,11 @@ func main () {
 		} else {
 			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 		}
+	})
+
+	designer := r.Group("/designer", TokenAuth())
+	designer.GET("/news", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"news": "hogehoge"})
 	})
 
 	r.GET("/readme", func(c *gin.Context) {
