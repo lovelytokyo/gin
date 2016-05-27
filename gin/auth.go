ca83e04e369c3f14bd80a6e2a4ad4970bb4a12ca
oh_minjung <oh_minjung@cyberagent.co.jp>
2016-05-27 14:19:25 +0900

    basic auth

---
+++ gin/auth.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var secrets = gin.H{
+	"foo": gin.H{
+		"email": "foo@bar.com",
+		"phone": "123433",
+	},
+	"austin": gin.H{
+		"email": "austin@bar.com",
+		"phone": "66666",
+	},
+}
+
+func main () {
+	r := gin.Default()
+
+	authorized := r.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo": "bar",
+		"austin": "1234",
+		"lena": "hello2",
+		"manu": "4321",
+	}))
+
+	authorized.GET("/secrets", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
+	})
+
+	r.Run(":8080")
+}
